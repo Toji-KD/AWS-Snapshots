f6c9497610224418455e3b92dd140fbb9347511b
Toji K Dominic <tojikdominic@gmail.com>
2017-07-08 04:19:59 +0530

    Update snapshot-from-specific-list.py

--- snapshot-from-specific-list.py
+++ snapshot-from-specific-list.py
@@ -5,9 +5,9 @@ import datetime
 import sys
 ##### Volume IDs #############
 #Specify voulume ids of instance volume to be backed up. Those should be comma seperated and kept in single quotes.
-Volume_list = ['vol-0bf7d84b29e915079','vol-00137057e16ee3954']  
+Volume_list = ['vol-0000000000','vol-000000001']  
 ###### Retention in days ######
-Retention = 0     #Specify retention here
+Retention = 4     #Specify retention here
 ###############################
 #This scipt takes SnapShots of specified volumes for the current region. Volume IDs should be specified above as list.
 #snapshots will be removed accoding to the retention period.
@@ -15,8 +15,8 @@ Retention = 0     #Specify retention here
 ##### Server Requirements ####################
 # AWS CLI - # pip install awscli , # aws configure
 # Python3 -   Default in all servers.
-# Pip     - # apt-get install -y python3-pip
-# Boto3   - # pip install boto3
+# Pip3     - # apt-get install -y python3-pip
+# Boto3   - # pip3 install boto3
 ##############################################
 # Written by Toji K Dominic 
 # tojikdominic@gmail.com +91 9747389586
@@ -58,7 +58,7 @@ for time in response['Snapshots']:
   time_now = datetime.datetime.now()
   D = time_now - snap_time
   Days=int(D.days)
-  if Days >= Retention:
+  if Days > Retention:
     try:
       del_snap = client.delete_snapshot(SnapshotId=time['SnapshotId'])
       if del_snap['ResponseMetadata']['HTTPStatusCode'] == 200:
